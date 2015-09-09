14f7984108063423a9fb770001ca7449984de949
grant lodge <grant.lodge@nesltd.co.uk>
2015-09-09 08:44:08 +0100

    game play

--- Loquacious/Loquacious.Game/SinglePlayerGame.cs
+++ Loquacious/Loquacious.Game/SinglePlayerGame.cs
@@ -8,6 +8,8 @@ namespace Loquacious.Game
 {
     public class SinglePlayerGame : IGame
     {
+        private Result _result;
+        
         public SinglePlayerGame()
         {
             Players = new List<IPlayer>() {new PlayerOne(),new ArtificalIntelligence()};
@@ -17,18 +19,46 @@ namespace Loquacious.Game
 
         public void StartGame()
         {
-            if (GameEnds != null)
-            {
-                GameEnds(Result.None, 0);
-            }
+            Task.Factory.StartNew(()=>{
+                
+                Thread.Sleep(1000);
+                CountDownTickOne();
+
+                ThreadSleep(1000);
+                CountDownTickTwo();
+
+                Thread.Sleep(1000);
+                CountDownTickGo();
+
+                Thread.Sleep(TimeAllowedForPicks);
+                    
+                if (GameEnds != null)
+                {
+                    GameEnds(Result.None, 0);
+                }
+                
+
+
+            });
         }
+
+
 
         public Action CountDownTickOne { get; set; }
         public Action CountDownTickTwo { get; set; }
         public Action CountDownTickGo { get; set; }
         public Action<Result, int> GameEnds { get; set; }
-        public Result Result { get; }
-        public double CountDown { get; }
-        public double TimeAllowedForPicks { get; }
+        public Result Result 
+        { 
+            get { return _result; }
+        }
+        public double CountDown 
+        { 
+            get { return 3000; } 
+        }
+        public double TimeAllowedForPicks 
+        { 
+            get{ return 1000; }
+        }
     }
 }
